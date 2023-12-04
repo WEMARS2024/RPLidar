33a0c461075a17fa19952aab07b40c14528bd158
Howard Wen <hwen46@uwo.ca>
2023-12-04 17:24:15 -0500

    dunn

--- lidar-computer/app.py
+++ lidar-computer/app.py
@@ -4,6 +4,7 @@ from flask_socketio import SocketIO, emit
 import random
 import threading
 import socket
+import numpy as np
 
 # Flask application setup
 app = Flask(__name__)
@@ -59,49 +60,58 @@ def get_path_data():
 # Background thread function to emit sensor data
 def send_sensor_data():
     with app.app_context():
+        debug = 1
         while True:
-            # Set up the server socket
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            host = '10.0.0.134'
-            port = 8080
+            if debug == 0:
+                # DEBUG
+                data_dict = {}
+                for x in range(0,360,30):
+                    data_dict[x+90] = np.random.uniform(0,12)
+                socketio.emit('sensor-data', data_dict)
+                socketio.sleep(2)
+            elif debug == 1:
+                # Set up the server socket
+                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                host = '172.30.122.204' # wireless LAN adapter
+                port = 8080
 
-            server_socket.bind((host, port))
-            server_socket.listen(1)
-            print(f"Server listening on {host}:{port}")
+                server_socket.bind((host, port))
+                server_socket.listen(1)
+                print(f"Server listening on {host}:{port}")
 
-            while True:
-                # Accept a client connection
-                client_socket, client_address = server_socket.accept()
-                print(f"Connection from {client_address}")
+                while True:
+                    # Accept a client connection
+                    client_socket, client_address = server_socket.accept()
+                    print(f"Connection from {client_address}")
 
-                # Receive data from the client
-                received_data = client_socket.recv(1024).decode()
-                if not received_data:
-                    print("Refused connection")
-                    continue
-                print(f"Received data from server: {received_data}")
-                
-                try:
-                    data_pairs = received_data.split(';')
-                    data_dict = {}
+                    # Receive data from the client
+                    received_data = client_socket.recv(1024).decode()
+                    if not received_data:
+                        print("Refused connection")
+                        continue
+                    print(f"Received data from server: {received_data}")
+                    
+                    try:
+                        data_pairs = received_data.split(';')
+                        data_dict = {}
 
-                    for pair in data_pairs:
-                        if ':' in pair:
-                            deg, dist = pair.split(':')
-                            try:
-                                deg = float(deg)
-                                dist = float(dist)
-                                data_dict[deg] = dist
-                            except ValueError:
-                                print(f"Invalid data pair: {pair}")
-                    print(data_dict)
-                    socketio.emit('sensor-data', data_dict)
-                    socketio.sleep(1)  # Use socketio.sleep instead of time.sleep
-                except ValueError:
-                    print("Error parsing data:", received_data)
-                
-                # Close the connection
-                client_socket.close()
+                        for pair in data_pairs:
+                            if ':' in pair:
+                                deg, dist = pair.split(':')
+                                try:
+                                    deg = float(deg)
+                                    dist = float(dist)
+                                    data_dict[deg] = dist
+                                except ValueError:
+                                    print(f"Invalid data pair: {pair}")
+                        print(data_dict)
+                        socketio.emit('sensor-data', data_dict)
+                        socketio.sleep(1)  # Use socketio.sleep instead of time.sleep
+                    except ValueError:
+                        print("Error parsing data:", received_data)
+                    
+                    # Close the connection
+                    client_socket.close()
 
 # SocketIO event handlers
 @socketio.on('connect')
